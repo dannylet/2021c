6ff0a6da785d5e3c5f13d5d820e3b650af56086b
黃建龍 <91250425+dannylet@users.noreply.github.com>
2021-10-28 17:39:34 +0800

    Add files via upload

---
+++ week07/week07-4.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    for (i=0; i<=3; i++)
+    {
+        printf ("課本的寫法哦\n");
+    }
+
+    for(int i=0; i<3; i++)
+    {
+        printf("小葉老師的寫法哦\n");
+    }
+}
