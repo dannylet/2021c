493d68307b05d6ac81be5cb283c791f50018d095
unknown <092zxc6632@gmail.com>
2021-12-30 16:50:29 +0800

    加入week16

---
+++ week16/week16-5.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int a[4];
+char line[300];
+int main()
+{
+    printf("請輸入你的字串:");
+    scanf("%s",line);
+    printf("剛剛輸入的是=%s=",line);
+
+    int ans=0;
+    int i=0;
+    while(line[i] !='\0')
+    {
+        if(line[i] == '1')ans++;
+
+        i++;
+    }
+    printf("你剛剛輸入了%d個1",ans);
+}
+
